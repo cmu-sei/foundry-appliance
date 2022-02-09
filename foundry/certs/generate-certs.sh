d0d7130a89d0043d4fe89c56d84f582508ec2202
Matt Kaar <66427159+sei-mkaar@users.noreply.github.com>
2022-02-08 21:39:12 -0500

    Lower cfssl log output during generate-certs.sh (#13)

--- foundry/certs/generate-certs.sh
+++ foundry/certs/generate-certs.sh
@@ -3,16 +3,19 @@
 # Copyright 2022 Carnegie Mellon University.
 # Released under a BSD (SEI)-style license, please see LICENSE.md in the
 # project root or contact permission@sei.cmu.edu for full terms.
+#
+# ./generate-certs.sh <gencert arguments>
 
+ARGS=$*
 
 # Change to the current directory
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # Generate root, intermediate and host certificates/keys
-cfssl gencert -initca root-ca.json | cfssljson -bare root-ca
-cfssl gencert -ca root-ca.pem -ca-key root-ca-key.pem -config config.json \
+cfssl gencert $ARGS -initca root-ca.json | cfssljson -bare root-ca
+cfssl gencert $ARGS -ca root-ca.pem -ca-key root-ca-key.pem -config config.json \
               -profile intca int-ca.json | cfssljson -bare int-ca
-cfssl gencert -ca int-ca.pem -ca-key int-ca-key.pem -config config.json \
+cfssl gencert $ARGS -ca int-ca.pem -ca-key int-ca-key.pem -config config.json \
               -profile server host.json | cfssljson -bare host
 
 # Create pkcs12 host bundle for identity signing key
